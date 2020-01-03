f5057f0c4d4b24fac13f70523d4741605eb0f463
Petro Protsakh <p.protsakh@scalr.com>
2020-01-03 17:45:06 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
-#!/bin/sh -l
+#!/bin/sh
 
-set -e
-/opa test ${INPUT_TESTS} -v
+IFS=';'
+mapfile -t lines <<< "$INPUT_TESTS"
+
+for line in "${lines[@]}"; do
+  read -r -a args <<< "$line"
+  cmd="/opa test ${args[@]} -v"
+  echo "Running: $cmd"
+  $cmd
+done
