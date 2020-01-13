73c31651cb22f4b5efa960ffdb1f6c668fe3ee27
Petro Protsakh <p.protsakh@scalr.com>
2020-01-13 17:43:57 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
 e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opa test ${args[*]}"
+  cmd="/opa test ${args[*]} $INPUT_OPTIONS"
   echo " 🚀 Running: $cmd"
   printf "\n"
   eval "$cmd" || e_code=1
