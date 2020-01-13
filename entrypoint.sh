ef60d2bc7ed5cb18109fa3786d87be30f2b32039
Petro Protsakh <p.protsakh@scalr.com>
2020-01-13 17:13:22 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,14 @@ printf "\n\n"
 IFS=';'
 mapfile -t lines <<< "$INPUT_TESTS"
 
+e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opa test ${args[@]} -v"
+  cmd="/opa test ${args[*]} -v"
   echo " 🚀 Running: $cmd"
   printf "\n"
-  eval "$cmd"
+  eval "$cmd" || e_code=1
   printf "\n\n"
 done
+
+exit $e_code
